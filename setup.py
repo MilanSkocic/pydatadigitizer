92e483b96224994efcaa793cc60ce9fed50a3fac
Milan Skocic <milan.skocic@gmail.com>
2021-03-10 22:23:09 +0100

    Changed import of version through the package directly.

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 import os
-import importlib.util
 from setuptools import setup, find_packages
-
-# local import of version.py file
-spec = importlib.util.spec_from_file_location('version', './datadigitizer/version.py')
-version = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(version)
+import datadigitizer
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname), 'r', encoding='utf-8').read()
 
 
-setup(name=version.__package_name__,
-      version=version.__version__,
-      maintainer=version.__maintainer__,
-      maintainer_email=version.__maintainer_email__,
-      author=version.__author__,
-      author_email=version.__author_email__,
-      description=version.__package_name__,
+setup(name=datadigitizer.__package_name__,
+      version=datadigitizer.__version__,
+      maintainer=datadigitizer.__maintainer__,
+      maintainer_email=datadigitizer.__maintainer_email__,
+      author=datadigitizer.__author__,
+      author_email=datadigitizer.__author_email__,
+      description=datadigitizer.__package_name__,
       long_description=read('README.rst'),
       url='https://github.com/MilanSkocic/datadigitizer',
       download_url='https://github.com/MilanSkocic/datadigitizer',
