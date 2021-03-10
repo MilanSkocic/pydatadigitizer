12e83d31e28a2f6fc6d4c7d977fce41ff8d9d02e
Milan Skocic <milan.skocic@gmail.com>
2021-03-10 22:22:00 +0100

    Added import of dunder names in datadigitizer from version.py.

--- datadigitizer/version.py
+++ datadigitizer/version.py
@@ -1,8 +1,17 @@
+__all__ = ['__package_name__',
+           '__version__',
+           '__author__',
+           '__author_email__',
+           '__maintainer__',
+           '__maintainer_email__',
+           '__copyright__',
+           '__license__']
+
 __package_name__ = "Data Digitizer"
 __version__ = "1.0.0"
 __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@gmail.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
-__copyright__ = 'Copyright (C) 2020, 2021 ' + __author__
+__copyright__ = 'Copyright (C) 2020-2021 ' + __author__
 __license__ = 'GNU General Public License v3 (GPLv3)'
