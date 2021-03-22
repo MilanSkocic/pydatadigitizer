7887a10b15179b04011977d74c8e8d2e3412e84b
Milan Skocic <milan.skocic@gmail.com>
2021-03-22 05:24:26 +0100

    MAINT: Reordering classes in gui.py.

--- datadigitizer/gui.py
+++ datadigitizer/gui.py
@@ -184,6 +184,194 @@ class FigureFrame(ttk.Frame):
         self.canvas.draw()
 
 
+class ScrolledFrame(ttk.Frame):
+    r"""Class for scrolled frames. See __init__.__doc__."""
+
+    def __init__(self, master, **kwargs):
+        r"""
+        Scrolled Frame widget which may contain other widgets and can have a 3D border.
+
+        Parameters
+        ------------
+        master: tkinter widget
+            Master container.
+        kwargs: dict, optional
+            Keyword arguments for the scrolled frame.
+        """
+        ttk.Frame.__init__(self, master)
+
+        self._default_options = {'scrolled': 'y'}
+
+        for i in kwargs.keys():
+            if i not in self._default_options.keys():
+                raise tk.TclError('Unknow option --' + i)
+
+        self._default_options.update(kwargs)
+
+        self.pack(expand=True, fill=tk.BOTH)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=0)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+
+        self.yscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
+        self.xscrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
+
+        if self._default_options['scrolled'] == 'y':
+            self.yscrollbar.grid(row=0, column=1, sticky='ns')
+        elif self._default_options['scrolled'] == 'x':
+            self.xscrollbar.grid(row=1, column=0, sticky='ew')
+        elif self._default_options['scrolled'] == 'both':
+            self.yscrollbar.grid(row=0, column=1, sticky='ns')
+            self.xscrollbar.grid(row=1, column=0, sticky='ew')
+        else:
+            raise tk.TclError('Bad scroll style \"' + self._default_options['scrolled'] + '\" must be x, y or both')
+
+        self._canvas = tk.Canvas(self, bd=0, relief=tk.FLAT,
+                                 yscrollcommand=self.yscrollbar.set,
+                                 xscrollcommand=self.xscrollbar.set)
+        self._canvas.grid(row=0, column=0, sticky='nswe')
+
+        self.yscrollbar.config(command=self._canvas.yview)
+        self.xscrollbar.config(command=self._canvas.xview)
+
+        self._canvas.config(scrollregion=self._canvas.bbox(tk.ALL))
+
+        self._frame = ttk.Frame(self._canvas)
+
+        self._canvas_window_id = self._canvas.create_window(0, 0, window=self._frame, anchor='nw')
+        self._canvas.itemconfig(self._canvas_window_id, width=self._frame.winfo_reqwidth())
+        self._canvas.bind("<Configure>", self._update_canvas_window_size)
+
+    def _update_canvas_window_size(self, event):
+        if event.width <= self._frame.winfo_reqwidth():
+            self._canvas.itemconfig(self._canvas_window_id, width=self._frame.winfo_reqwidth())
+        else:
+            self._canvas.itemconfig(self._canvas_window_id, width=event.width)
+
+        if event.height <= self._frame.winfo_reqheight():
+            self._canvas.itemconfig(self._canvas_window_id, height=self._frame.winfo_reqheight())
+        else:
+            self._canvas.itemconfig(self._canvas_window_id, height=event.height)
+
+        self._update_canvas_bbox()
+
+    def _update_canvas_bbox(self):
+        self._canvas.config(scrollregion=self._canvas.bbox(tk.ALL))
+
+    @property
+    def frame(self):
+        r"""Return the frame that contains the widgets."""
+        return self._frame
+
+    @property
+    def canvas(self):
+        r"""Return the canvas that contains the scrollbars."""
+        return self._canvas
+
+
+class AboutWindow(tk.Toplevel):
+    r"""Class for about window. See __init__.__doc__."""
+    def __init__(self, master):
+        r"""
+        About window.
+
+        Parameters
+        ----------
+        master: tkinter widget
+            Container.
+        """
+        super().__init__(master)
+        self.transient(master)
+
+        self.master = master
+        self.title('About')
+
+        self.grab_set()
+
+        self.initial_focus = self
+
+        self.protocol("WM_DELETE_WINDOW", self._quit)
+
+        ws = self.master.winfo_screenwidth()
+        hs = self.master.winfo_screenheight()
+        width = int(0.65*ws)
+        height = int(0.1*hs)
+        x = int((ws / 2) - (width / 2))
+        y = int((hs / 2) - (height / 2) - 25)
+        self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        self.resizable(height=False, width=False)
+
+        self.frame = ttk.Frame(self)
+        self.frame.pack(fill=tk.BOTH, expand=tk.TRUE)
+
+        for i in range(2):
+            self.frame.grid_rowconfigure(i, weight=1)
+        for i in range(1):
+            self.frame.grid_columnconfigure(i, weight=1)
+
+        msg = version.__package_name__ + ': ' + version.__version__
+        label = ttk.Label(self.frame, text=msg)
+        label.configure(anchor='center')
+        label.grid(row=0, column=0, sticky='nswe')
+
+        msg = 'Python : ' + sys.version.replace('\n', ' ')
+        label = ttk.Label(self.frame, text=msg)
+        label.configure(anchor='center')
+        label.grid(row=1, column=0, sticky='nswe')
+
+        self.initial_focus.focus_set()
+        self.wait_window(self)
+
+    def _quit(self):
+        self.master.focus_set()
+        self.destroy()
+
+
+class HowToUse(tk.Toplevel):
+    r"""Class for quick help window. See __init__.__doc__."""
+    def __init__(self, master):
+        r"""
+        How to use window.
+
+        Parameters
+        ----------
+        master: tkinter widget
+            Container.
+        """
+        super().__init__(master)
+        self.transient(master)
+
+        self.master = master
+        self.title('How To Use')
+
+        self.grab_set()
+
+        self.initial_focus = self
+
+        self.protocol("WM_DELETE_WINDOW", self._quit)
+
+        ws = self.master.winfo_screenwidth()
+        hs = self.master.winfo_screenheight()
+        width = int(0.5*ws)
+        height = int(0.7*hs)
+        x = int((ws / 2) - (width / 2))
+        y = int((hs / 2) - (height / 2) - 25)
+        self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+        kwargs = {'scrolled': 'both'}
+        self.sframe = ScrolledFrame(self, **kwargs)
+
+        msg = self.master.__init__.__doc__.split('Parameters')[0]
+        label = ttk.Label(self.sframe.frame, text=msg)
+        label.configure(anchor='w', justify='left')
+        label.grid(row=0, column=0, sticky='nswe')
+
+    def _quit(self):
+        self.master.focus_set()
+        self.destroy()
+
+
 class App(ttk.Frame):
     r"""Class for main graphical interface. See __init__.__doc__."""
 
@@ -954,191 +1142,3 @@ class App(ttk.Frame):
         Start the application.
         """
         self.mainloop()
-
-
-class ScrolledFrame(ttk.Frame):
-    r"""Class for scrolled frames. See __init__.__doc__."""
-
-    def __init__(self, master, **kwargs):
-        r"""
-        Scrolled Frame widget which may contain other widgets and can have a 3D border.
-
-        Parameters
-        ------------
-        master: tkinter widget
-            Master container.
-        kwargs: dict, optional
-            Keyword arguments for the scrolled frame.
-        """
-        ttk.Frame.__init__(self, master)
-
-        self._default_options = {'scrolled': 'y'}
-
-        for i in kwargs.keys():
-            if i not in self._default_options.keys():
-                raise tk.TclError('Unknow option --' + i)
-
-        self._default_options.update(kwargs)
-
-        self.pack(expand=True, fill=tk.BOTH)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=0)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=0)
-
-        self.yscrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
-        self.xscrollbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL)
-
-        if self._default_options['scrolled'] == 'y':
-            self.yscrollbar.grid(row=0, column=1, sticky='ns')
-        elif self._default_options['scrolled'] == 'x':
-            self.xscrollbar.grid(row=1, column=0, sticky='ew')
-        elif self._default_options['scrolled'] == 'both':
-            self.yscrollbar.grid(row=0, column=1, sticky='ns')
-            self.xscrollbar.grid(row=1, column=0, sticky='ew')
-        else:
-            raise tk.TclError('Bad scroll style \"' + self._default_options['scrolled'] + '\" must be x, y or both')
-
-        self._canvas = tk.Canvas(self, bd=0, relief=tk.FLAT,
-                                 yscrollcommand=self.yscrollbar.set,
-                                 xscrollcommand=self.xscrollbar.set)
-        self._canvas.grid(row=0, column=0, sticky='nswe')
-
-        self.yscrollbar.config(command=self._canvas.yview)
-        self.xscrollbar.config(command=self._canvas.xview)
-
-        self._canvas.config(scrollregion=self._canvas.bbox(tk.ALL))
-
-        self._frame = ttk.Frame(self._canvas)
-
-        self._canvas_window_id = self._canvas.create_window(0, 0, window=self._frame, anchor='nw')
-        self._canvas.itemconfig(self._canvas_window_id, width=self._frame.winfo_reqwidth())
-        self._canvas.bind("<Configure>", self._update_canvas_window_size)
-
-    def _update_canvas_window_size(self, event):
-        if event.width <= self._frame.winfo_reqwidth():
-            self._canvas.itemconfig(self._canvas_window_id, width=self._frame.winfo_reqwidth())
-        else:
-            self._canvas.itemconfig(self._canvas_window_id, width=event.width)
-
-        if event.height <= self._frame.winfo_reqheight():
-            self._canvas.itemconfig(self._canvas_window_id, height=self._frame.winfo_reqheight())
-        else:
-            self._canvas.itemconfig(self._canvas_window_id, height=event.height)
-
-        self._update_canvas_bbox()
-
-    def _update_canvas_bbox(self):
-        self._canvas.config(scrollregion=self._canvas.bbox(tk.ALL))
-
-    @property
-    def frame(self):
-        r"""Return the frame that contains the widgets."""
-        return self._frame
-
-    @property
-    def canvas(self):
-        r"""Return the canvas that contains the scrollbars."""
-        return self._canvas
-
-
-class AboutWindow(tk.Toplevel):
-    r"""Class for about window. See __init__.__doc__."""
-    def __init__(self, master):
-        r"""
-        About window.
-
-        Parameters
-        ----------
-        master: tkinter widget
-            Container.
-        """
-        super().__init__(master)
-        self.transient(master)
-
-        self.master = master
-        self.title('About')
-
-        self.grab_set()
-
-        self.initial_focus = self
-
-        self.protocol("WM_DELETE_WINDOW", self._quit)
-
-        ws = self.master.winfo_screenwidth()
-        hs = self.master.winfo_screenheight()
-        width = int(0.65*ws)
-        height = int(0.1*hs)
-        x = int((ws / 2) - (width / 2))
-        y = int((hs / 2) - (height / 2) - 25)
-        self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-        self.resizable(height=False, width=False)
-
-        self.frame = ttk.Frame(self)
-        self.frame.pack(fill=tk.BOTH, expand=tk.TRUE)
-
-        for i in range(2):
-            self.frame.grid_rowconfigure(i, weight=1)
-        for i in range(1):
-            self.frame.grid_columnconfigure(i, weight=1)
-
-        msg = version.__package_name__ + ': ' + version.__version__
-        label = ttk.Label(self.frame, text=msg)
-        label.configure(anchor='center')
-        label.grid(row=0, column=0, sticky='nswe')
-
-        msg = 'Python : ' + sys.version.replace('\n', ' ')
-        label = ttk.Label(self.frame, text=msg)
-        label.configure(anchor='center')
-        label.grid(row=1, column=0, sticky='nswe')
-
-        self.initial_focus.focus_set()
-        self.wait_window(self)
-
-    def _quit(self):
-        self.master.focus_set()
-        self.destroy()
-
-
-class HowToUse(tk.Toplevel):
-    r"""Class for quick help window. See __init__.__doc__."""
-    def __init__(self, master):
-        r"""
-        How to use window.
-
-        Parameters
-        ----------
-        master: tkinter widget
-            Container.
-        """
-        super().__init__(master)
-        self.transient(master)
-
-        self.master = master
-        self.title('How To Use')
-
-        self.grab_set()
-
-        self.initial_focus = self
-
-        self.protocol("WM_DELETE_WINDOW", self._quit)
-
-        ws = self.master.winfo_screenwidth()
-        hs = self.master.winfo_screenheight()
-        width = int(0.5*ws)
-        height = int(0.7*hs)
-        x = int((ws / 2) - (width / 2))
-        y = int((hs / 2) - (height / 2) - 25)
-        self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-
-        kwargs = {'scrolled': 'both'}
-        self.sframe = ScrolledFrame(self, **kwargs)
-
-        msg = self.master.__init__.__doc__.split('Parameters')[0]
-        label = ttk.Label(self.sframe.frame, text=msg)
-        label.configure(anchor='w', justify='left')
-        label.grid(row=0, column=0, sticky='nswe')
-
-    def _quit(self):
-        self.master.focus_set()
-        self.destroy()
