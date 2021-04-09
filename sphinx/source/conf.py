92d39e3862f6efd636b4fd1f8715131d10f618f8
MilanSkocic <milan.skocic@gmail.com>
2021-04-09 21:09:42 +0200

    DOC: Fixed error in the version number for online documentation.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -56,7 +56,7 @@ author = 'Milan Skocic'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '1.0.0-rc1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
