f98bfc2eb76fd67bce0ab75262dfe60298851564
MilanSkocic <milan.skocic@gmail.com>
2021-04-12 17:41:39 +0200

    REL: Preparing release 1.0.0-rc2.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -56,7 +56,7 @@ author = 'Milan Skocic'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0-rc1'
+version = '1.0.0-rc2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
