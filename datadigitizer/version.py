a9ced431ff8e54400940642de4828d6e7e44fb5f
MilanSkocic <milan.skocic@gmail.com>
2021-10-11 05:48:19 +0200

    REL: Prepared new version for doc and setup.

--- datadigitizer/version.py
+++ datadigitizer/version.py
@@ -28,7 +28,7 @@ __all__ = ['__package_name__',
            '__license__']
 
 __package_name__ = "datadigitizer"
-__version__ = "dev"
+__version__ = "1.0.2"
 __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@gmail.com"
 __maintainer__ = __author__
