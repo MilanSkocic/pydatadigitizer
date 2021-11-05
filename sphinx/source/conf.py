9ead2191f02baceff0c65c093633d58b8508d037
MilanSkocic <milan.skocic@gmail.com>
2021-11-05 05:41:00 +0100

    REL: Change version number.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -56,7 +56,7 @@ author = 'Milan Skocic'
 # built documents.
 #
 # The short X.Y version.
-version = 'X.Y.Z'
+version = "1.1.0" 
 # The full version, including alpha/beta/rc tags.
 release = version
 
