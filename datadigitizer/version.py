99f5e69615029a5aeb5e8d87de51e5ee83139a25
Milan Skocic <milan.skocic@gmail.com>
2022-03-03 09:31:42 +0100

    RLS: Automated the insertion of the package vesion in documentation.

--- datadigitizer/version.py
+++ datadigitizer/version.py
@@ -33,5 +33,5 @@ __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@gmail.com"
 __maintainer__ = __author__
 __maintainer_email__ = __author_email__
-__copyright__ = 'Copyright (C) 2020-2021 ' + __author__
+__copyright__ = 'Copyright (C) 2020-2022 ' + __author__
 __license__ = 'GNU General Public License v3 (GPLv3)'
