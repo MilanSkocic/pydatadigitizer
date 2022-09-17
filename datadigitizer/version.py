d87b8a5cef2ddae68c9f512ed0a9667c094b891e
Milan Skocic <milan.skocic@icloud.com>
2022-09-17 09:11:01 +0200

    RLS: Change versio for next release.

--- datadigitizer/version.py
+++ datadigitizer/version.py
@@ -28,7 +28,7 @@ __all__ = ['__package_name__',
            '__license__']
 
 __package_name__ = "datadigitizer"
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@gmail.com"
 __maintainer__ = __author__
